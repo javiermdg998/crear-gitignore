9018a6704bf81e70a53d9218bc14d96c78f5c6a4
javier <javiermdg998@gmail.com>
2018-10-15 18:30:49 +0200

    acabado

--- crear_gitignore.ps1
+++ crear_gitignore.ps1
@@ -2,27 +2,22 @@ $path=".gitignore"
 $idea=".idea"
 if(![System.IO.File]::Exists($path)){
    gibo dump windows linux macos > .gitignore
-   "gitignore general creado"
 }
 
 Foreach($i in Get-ChildItem (Get-Location)){
-    if(Test-Path -Path $i.fullname -PathType Container){
-        #$i.fullname
+    if(Test-Path -Path $i.fullname -PathType Container){      
         Foreach($d in Get-ChildItem($i.fullname)){
-             if(Test-Path -Path $d.fullname -PathType Container){
-                 
-               #  "check"
-                 #"$($i.fullname)\$idea"
-                 if(Test-Path -Path  "$($i.fullname)\$idea"){
-                    $fullname="$($i.fullname)/$path"
-                    
-                    if(![System.IO.File]::Exists($fullname)){
-                        gibo dump java jetbrains > "$fullname"
-                        "gitignore creado"
+             if(Test-Path -Path $d.fullname -PathType Container){              
+                    if(Test-Path -Path  "$($d.fullname)\$idea"){
+                        $fullname="$($d.fullname)/$path"                       
+                        if(![System.IO.File]::Exists($fullname)){
+                            gibo dump java jetbrains > "$fullname"
+                                                      
+                        }
                     }
                 }
                                
-            }
+            
         }
     }
 }
