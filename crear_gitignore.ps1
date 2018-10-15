67fe0044a4e68d6c7747edd672c5e94d09c23014
javier <javiermdg998@gmail.com>
2018-10-15 12:09:31 +0200

    arreglos, comprobar si hay que crear a segundo nivel

--- crear_gitignore.ps1
+++ crear_gitignore.ps1
@@ -2,22 +2,23 @@ $path=".gitignore"
 $idea=".idea"
 if(![System.IO.File]::Exists($path)){
    gibo dump windows linux macos > .gitignore
+   "gitignore general creado"
 }
 
 Foreach($i in Get-ChildItem (Get-Location)){
     if(Test-Path -Path $i.fullname -PathType Container){
-        $i.fullname
+        #$i.fullname
         Foreach($d in Get-ChildItem($i.fullname)){
              if(Test-Path -Path $d.fullname -PathType Container){
                  
-                 "check"
-                 "$($i.fullname)\$idea"
+               #  "check"
+                 #"$($i.fullname)\$idea"
                  if(Test-Path -Path  "$($i.fullname)\$idea"){
                     $fullname="$($i.fullname)/$path"
-                    "he llegado aqui 2"
+                    
                     if(![System.IO.File]::Exists($fullname)){
                         gibo dump java jetbrains > "$fullname"
-                        "he llegado aqui"
+                        "gitignore creado"
                     }
                 }
                                
